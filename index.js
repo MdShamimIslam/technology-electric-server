4d83a9627f2f8747ee0e9a3f91639b315dd46930
MdShamimIslam <shamim401897@gmail.com>
2023-12-04 14:54:03 +0600

    client reviews collection created

--- index.js
+++ index.js
@@ -20,11 +20,12 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    client.connect();
 
     const brandCollection = client.db("TechDb").collection("brands");
     const productCollection = client.db("TechDb").collection("products");
     const cartCollection = client.db("TechDb").collection("carts");
+    const reviewCollection = client.db("TechDb").collection("reviews");
 
     // brands related apis
     app.get("/brands", async (req, res) => {
@@ -58,47 +59,55 @@ async function run() {
       res.send(result);
     });
 
-    app.put('/products/:id', async(req,res)=>{
+    app.put("/products/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = { _id : new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) };
       const updateProduct = req.body;
       const options = { upsert: true };
       const product = {
         $set: {
-          productName: updateProduct.productName, 
-          productImg: updateProduct.productImg, 
-          brandName: updateProduct.brandName, 
-          type: updateProduct.type, 
-          price: updateProduct.price, 
-          rating: updateProduct.rating, 
-          description: updateProduct. description
-        }
-    }
+          productName: updateProduct.productName,
+          productImg: updateProduct.productImg,
+          brandName: updateProduct.brandName,
+          type: updateProduct.type,
+          price: updateProduct.price,
+          rating: updateProduct.rating,
+          description: updateProduct.description,
+        },
+      };
 
-    const result = await productCollection.updateOne(filter, product, options);
-    res.send(result);
-    })
+      const result = await productCollection.updateOne(
+        filter,
+        product,
+        options
+      );
+      res.send(result);
+    });
 
     // carts related apis
-    app.get('/carts', async(req,res)=>{
+    app.get("/carts", async (req, res) => {
       const result = await cartCollection.find().toArray();
       res.send(result);
-    })
+    });
 
-    app.post('/carts',async(req,res)=>{
+    app.post("/carts", async (req, res) => {
       const product = req.body;
       const result = await cartCollection.insertOne(product);
       res.send(result);
-    })
+    });
 
-    app.delete('/carts/:id', async(req,res)=>{
+    app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id : new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
-   
+    // reviews related apis
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
